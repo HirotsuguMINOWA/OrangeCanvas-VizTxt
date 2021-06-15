38dcca447e9d58a8b20a0126d5e5506de18a5e27
Hirotsugu MINOWA <minowa.hirotsugu@gmail.com>
2021-06-16 01:19:05 +0900

    Create new widget that it filters to corpus by regexp. And example and csv for it.

---
+++ full_run_with_kokoro_parse.sh
@@ -0,0 +1,2 @@
+#pip uninstall JapaneseOC -y && python setup.py install && python -m Orange.canvas --force-discovery --clear-widget-settings
+pip uninstall NLP4OC -y && python setup.py install && python -m Orange.canvas --clear-widget-settings orangecontrib/nlp/tutorials/kokoro_parse.ows
